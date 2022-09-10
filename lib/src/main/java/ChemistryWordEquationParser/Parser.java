575efd99aedca66109ce38df54a9ab1d2281f6ce
Thanatoslayer6 <Thanatoslayer6@gmail.com>
2022-09-10 19:16:57 +0800

    Working code pushed

--- lib/src/main/java/ChemistryWordEquationParser/Parser.java
+++ lib/src/main/java/ChemistryWordEquationParser/Parser.java
@@ -36,18 +36,8 @@ public class Parser {
 		System.out.println(Arrays.toString(ProductFirstSolved));
 
 		// Second check if there are ions needed to be fixed, check for spaces
-		
+		// System.out.println("The parsed stuff is: " + Arrays.toString(Parser.secondSolve(ProductFirstSolved)));
 
-		/*
-		int info[] = Parser.getIonIndex(UnparsedEquation);
-		System.out.println("The indexes are:" + Arrays.toString(info));
-		// Output the elements
-		for (int i = 0; i < info.length; i++) {
-			System.out.println("The name of the element is: " + mm.ions[info[i]][0]);
-			System.out.println("The symbol of the element is: " + mm.ions[info[i]][1]);
-			System.out.println("The charge of the element is: " + mm.ions[info[i]][2]);
-		}
-		*/
 	}
 
 	public static String[] firstSolve(String side[]) {
@@ -63,22 +53,87 @@ public class Parser {
 	
 	// Whitespaces should be eliminated by now...
 	public static String[] secondSolve(String side[]) {
-		String TwoIons[];
-		int leftIonIndex, rightIonIndex, leftIonCharge, rightIonCharge;
+		Boolean leftIonEndsWithNum, rightIonEndsWithNum;
+		String TwoIons[], leftIon, rightIon;
+		int leftIonIndex, rightIonIndex, leftIonCharge, rightIonCharge, calculatedGcd;
 		for (int i = 0; i < side.length; i++) {
 			if (side[i].contains(" ")) {
 				// First split where index 0 is the left ion and 1 is the right
 				TwoIons = side[i].split(" ");
-				// Get the charges of the ions
+				// Get the charges of the ions, make sure anion is positive (see abs value *)
 				leftIonIndex = mm.FindMetalIndex(TwoIons[0]);
 				rightIonIndex = mm.FindMetalIndex(TwoIons[1]);
-				leftIonCharge = Integer.parseInt(mm.ions[leftIonIndex][2]);
-				rightIonCharge = Integer.parseInt(mm.ions[rightIonIndex][2]);
-				// 
-				//+1, -3
-				if (leftIonCharge )
+				leftIonCharge = Integer.parseInt(mm.ions[leftIonIndex][2]); // Cation charge
+				rightIonCharge = Integer.parseInt(mm.ions[rightIonIndex][2]); // Anion charge
+				leftIon = mm.ions[leftIonIndex][1]; // Cation
+				rightIon = 	mm.ions[rightIonIndex][1]; // Anion
+
+				/* If right ion/anion is positive, then we can say that its the user's fault
+				 * TODO: Implement error check when right ion is positive or when user enters
+				 * something wrong...
+				 */
+				/*
+				 *  For now, take +1, and -3 as an example for cation and anion then we can simply
+				 *  just apply the 'criss-cross' method (just replace the charges). However, if it is +2, and -4 then we must
+				 *  find the gcd first then simplify or divide the values of the cation and anion by it
+				 */
+
+				// First calculate the gcd
+				calculatedGcd = gcd(leftIonCharge, Math.abs(rightIonCharge));
+				// Take the absolute value of the anion
+				rightIonCharge = Math.abs(rightIonCharge);
+				// Divide the gcd by the left and right charges
+				leftIonCharge /= calculatedGcd;
+				rightIonCharge /= calculatedGcd;
+				// Check if ions end with a number
+				leftIonEndsWithNum = Character.isDigit(leftIon.charAt(leftIon.length() - 1));
+				rightIonEndsWithNum = Character.isDigit(rightIon.charAt(rightIon.length() - 1));
+
+				// Just apply the 'criss-cross' method, simply switch the charges
+				if (rightIonCharge == 1 && leftIonCharge == 1) {
+					side[i] = leftIon + rightIon;
+				} else if (rightIonCharge == 1 && leftIonCharge != 1) {
+					if (rightIonEndsWithNum) {
+						side[i] = leftIon + "(" + rightIon + ")" + leftIonCharge;
+					} else {
+						side[i] = leftIon + rightIon + leftIonCharge;
+					}
+				} else if (leftIonCharge == 1 && rightIonCharge != 1) {	
+					if (leftIonEndsWithNum) {
+						side[i] = "(" + leftIon + ")" + rightIonCharge + rightIon;
+					} else {
+						side[i] = leftIon + rightIonCharge + rightIon;
+					}
+				} else { 
+					if (leftIonEndsWithNum && rightIonEndsWithNum) {
+						// If for example the two ions have charges > 1 like Sb2Cr2O75
+						side[i] = "(" + leftIon + ")" + rightIonCharge + "(" + rightIon + ")" + leftIonCharge;
+					} else if (leftIonEndsWithNum && !rightIonEndsWithNum) {
+						side[i] = "(" + leftIon + ")" + rightIonCharge +  rightIon + leftIonCharge;
+					} else if (!leftIonEndsWithNum && rightIonEndsWithNum) {
+						side[i] = leftIon + rightIonCharge + "(" +  rightIon + ")" + leftIonCharge;
+					} else {
+						side[i] = leftIon + rightIonCharge + rightIon + leftIonCharge;
+					}
+				}	
 			}
 		}
+		return side;
+	}
+	/**
+	 * Returns the greatest common divisor, useful for simplifying fractions...
+	 * @param n1 first number
+	 * @param n2 second number
+	 * @return gcd
+	 */
+	public static int gcd(int n1, int n2) {
+		int gcd = 1;
+		for (int i = 1; i <= n1 && i <= n2; ++i) {
+			if (n1 % i == 0 && n2 % i == 0) {
+				gcd = i;
+			}
+		}
+		return gcd;
 	}
 	/**
 	First split the equation into two where the index of 0 is the reactant and 1
